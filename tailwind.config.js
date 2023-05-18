d8b59cb02e3a830bc19324f7d6b50d3521c303b0
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2023-05-18 13:16:59 -0500

    Add styling to desktop header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./*.{html,js"],
+  content: ["./*.html","./*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        project_primary: "#324E7B",
+        project_secondary: "#86A6DF",
+        project_tertiary: "#5068A9",
+        project_white: "#F8F8F8",
+      },
+    },
   },
   plugins: [],
-}
-
+};
