0e91a197e4627bd1ddad624410ef61d035a59e07
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2023-05-23 08:38:57 -0500

    Add toggle to hamburger menu in mobile

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 import "./style.css";
 
+const mobileMenuList = document.getElementById("mobileMenuList");
+const mobileMenuIcon = document.getElementById("mobileMenuIcon");
+const menuGraph = document.getElementById("menu-graph");
+
+mobileMenuIcon.addEventListener("click", (evt) => {
+  mobileMenuList.classList.toggle("hidden");
+  mobileMenuList.classList.toggle("mobile-menu");
+  menuGraph.classList.toggle("fa-bars");
+  menuGraph.classList.toggle("fa-xmark");
+});
